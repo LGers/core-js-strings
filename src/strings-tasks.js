7f0a0025cb155d79a9d03b1ddace99598e01f0af
Leonid Mychko <lgers@mail.ru>
2023-12-25 12:13:16 +0300

    feat: implement reverseWords

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -402,8 +402,11 @@ function findLongestWord(sentence) {
  *   reverseWords('Hello World') => 'olleH dlroW'
  *   reverseWords('The Quick Brown Fox') => 'ehT kciuQ nworB xoF'
  */
-function reverseWords(/* str */) {
-  throw new Error('Not implemented');
+function reverseWords(str) {
+  return str
+    .split(' ')
+    .map((w) => [...w].reverse().join(''))
+    .join(' ');
 }
 
 /**
