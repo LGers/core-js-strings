99cb99ac0bf4a7ac08e721e5e0c9aeda620334b5
Leonid Mychko <lgers@mail.ru>
2023-12-25 10:28:15 +0300

    feat: implement repeatString

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -142,8 +142,12 @@ function removeTrailingWhitespaces(value) {
  *   repeatString('', 3) => ''
  *   repeatString('abc', -2) => ''
  */
-function repeatString(/* str, times */) {
-  throw new Error('Not implemented');
+function repeatString(str, times) {
+  if (times > 0) {
+    return str + repeatString(str, times - 1);
+  }
+
+  return '';
 }
 
 /**
